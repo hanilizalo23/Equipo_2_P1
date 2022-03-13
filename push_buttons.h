9adab23773e3da2e2a615e26eab4d18f0d1cfdf9
Mauricio Peralta O <mperalta.osorio@iteso.mx>
2022-03-12 22:03:40 -0600

    Some declarations and functions without the content

--- push_buttons.h
+++ push_buttons.h
@@ -4,8 +4,12 @@
  * @file    push_buttons.h
  */
 
-
 #ifndef PUSH_BUTTONS_H_
 #define PUSH_BUTTONS_H_
+
+void push_buttons_init(void); /**Configuration for all the push buttons*/
+void change_freq (void); /**When external buttons are pressed*/
+void buttons_pressed(void); /**Default signal when system runs for the first time*/
+void internal_sw_pressed(void); /**Changing the signals*/
 
 #endif /* PUSH_BUTTONS_H_*/
