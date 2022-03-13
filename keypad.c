7dab1e7f6514d9bfdf5cc7c415613609088eb6b9
Mauricio Peralta O <mperalta.osorio@iteso.mx>
2022-03-13 16:50:53 -0600

    Completed fuctions but not working yet

--- keypad.c
+++ keypad.c
@@ -17,11 +17,13 @@
 #define SYSTEM_CLOCK 	(21000000U)
 #define LIMIT_TO_TURN_OFF_LED (19U)
 
-/**Configuration of output pins as known with passive filter to avoid rebounds*/
+/**Configuration of output pins as known with passive filter  to avoid rebounds*/
 gpio_pin_control_register_t pin_control_register_gpiob_keypad_rows_inputs = GPIO_MUX1 | GPIO_PFE | INTR_FALLING_EDGE;
 gpio_pin_control_register_t pin_control_register_gpiod_keypad_columns_outputs = GPIO_MUX1;
 
 static uint8_t g_key = NO_KEY;
+static uint32_t g_col = COLUMN0;
+static uint8_t g_key_pressed = FALSE;
 
 void keypad_init(void)
 {
@@ -73,17 +75,193 @@ void keypad_init(void)
 
 void change_column(void) /**This function change the column every 500 mS*/
 {
+	static uint8_t turn_off_led = 0;
+	g_key_pressed = TRUE;
+	g_col = next_column(g_col);
+	GPIO_clear_pin(GPIO_D, g_col);
 
+	/**For turning off the LED 1 second after it that was turned on*/
+	if(GPIO_read_pin(GPIO_B, bit_22) || GPIO_read_pin(GPIO_E, bit_26))
+	{
+		turn_off_led++;
+		if(LIMIT_TO_TURN_OFF_LED == turn_off_led)
+		{
+			GPIO_set_pin(GPIO_B, bit_22);
+			GPIO_set_pin(GPIO_E, bit_26);
+			turn_off_led = NOTHING;
+		}
+	}
 }
 
 uint32_t next_column(uint32_t col) /**This functions makes a sweep of the column to be on, after the one who was off*/
 {
+	switch(col)
+	{
+		case COLUMN0:
+			GPIO_set_pin(GPIO_D, bit_0);
+			return COLUMN1;
+		break;
 
+		case COLUMN1:
+			GPIO_set_pin(GPIO_D, bit_1);
+			return COLUMN2;
+		break;
+
+		case COLUMN2:
+			GPIO_set_pin(GPIO_D, bit_2);
+			return COLUMN3;
+		break;
+
+		case COLUMN3:
+			GPIO_set_pin(GPIO_D, bit_3);
+			return COLUMN0;
+		break;
+
+		default:
+			return COLUMN0;
+	}
+	return FALSE;
+}
+
+uint8_t get_row(void) /**Saves the row in which was pressed a key*/
+{
+	static uint8_t row = NO_ROW;
+
+	if(!GPIO_read_pin(GPIO_B, bit_2))
+	{
+		row = ROW_0;
+		return row;
+	}
+	else if(!GPIO_read_pin(GPIO_B, bit_3))
+	{
+		row = ROW_1;
+		return row;
+	}
+	else if(!GPIO_read_pin(GPIO_B, bit_10))
+	{
+		row = ROW_2;
+		return row;
+	}
+	else if(!GPIO_read_pin(GPIO_B, bit_11))
+	{
+		row = ROW_3;
+		return row;
+	}
+	else
+	{
+		row = NO_ROW;
+	}
+
+	return NO_ROW;
 }
 
 void get_pressed_key(void) /**Returns in the global variable the value of the pressed key, knowing the row and the column*/
 {
+	static uint8_t row = NO_ROW;
+	row = get_row();
 
+	if(TRUE == g_key_pressed)
+	{
+		switch(g_col)
+		{
+			case COLUMN0:
+				if(ROW_0 == row)
+				{
+					g_key = KEY_ONE;
+				}
+				else if(ROW_1 == row)
+				{
+					g_key = KEY_FOUR;
+				}
+				else if(ROW_2 == row)
+				{
+					g_key = KEY_SEVEN;
+				}
+				else if(ROW_3 == row)
+				{
+					g_key = KEY_STAR;
+				}
+				else
+				{
+					g_key = NO_KEY;
+				}
+			break;
+
+			case COLUMN1:
+				if(ROW_0 == row)
+				{
+					g_key = KEY_TWO;
+				}
+				else if(ROW_1 == row)
+				{
+					g_key = KEY_FIVE;
+				}
+				else if(ROW_2 == row)
+				{
+					g_key = KEY_EIGHT;
+				}
+				else if(ROW_3 == row)
+				{
+					g_key = KEY_ZERO;
+				}
+				else
+				{
+					g_key = NO_KEY;
+				}
+			break;
+
+			case COLUMN2:
+				if(ROW_0 == row)
+				{
+					g_key = KEY_THREE;
+				}
+				else if(ROW_1 == row)
+				{
+					g_key = KEY_SIX;
+				}
+				else if(ROW_2 == row)
+				{
+					g_key = KEY_NINE;
+				}
+				else if(ROW_3 == row)
+				{
+					g_key = KEY_HASH;
+				}
+				else
+				{
+					g_key = NO_KEY;
+				}
+			break;
+
+			case COLUMN3:
+				if(ROW_0 == row)
+				{
+					g_key = KEY_A;
+				}
+				else if(ROW_1 == row)
+				{
+					g_key = KEY_B;
+				}
+				else if(ROW_2 == row)
+				{
+					g_key = KEY_C;
+				}
+				else if(ROW_3 == row)
+				{
+					g_key = KEY_D;
+				}
+				else
+				{
+					g_key = NO_KEY;
+				}
+			break;
+
+			default:
+				g_key = NO_KEY;
+			break;
+		}
+	}
+	g_key_pressed = FALSE;
 }
 
 uint8_t read_pressed_key(void) //*A getter for the pressed key*/
